d8353060ae9f304afdf09f00891925d03e22b6c7
Daniele Ciccarelli <da.ciccarelli@lottomatica.com>
2024-05-27 15:05:49 +0200

    Aggiungere i file di progetto.

---
+++ CiccazonWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CiccazonWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
